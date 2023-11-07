54694bde00ef513747dea5c4bb3f30f954369ff9
Gilberto de Freitas <gilberto@frgnet.com.br>
2023-11-07 15:01:59 -0300

    added shadcn

--- src/layout/topbar.tsx
+++ src/layout/topbar.tsx
@@ -1,3 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export const Topbar = () => {
-    return <section className="w-full bg-red-500 h-16">topbar</section>
-}
+  return (
+    <section className="w-full h-16 flex justify-between items-center p-5">
+      <div>a</div>
+      <div>
+        <Button variant='default' size="icon">b</Button>
+      </div>
+    </section>
+  );
+};
